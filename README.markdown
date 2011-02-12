29f02f9798469f1daf2a748a7092bb33c4c9a972
Lasse V. Karlsen <lasse@vkarlsen.no>
2011-02-12 16:38:49 +0100

    Initial commit, empty projects.

---
+++ README.markdown
@@ -0,0 +1,14 @@
+This project contains code to host a WebDAV server inside your own
+software, and in the future, code to access a WebDAV server.
+
+Repository and project location: [CodePlex][1]  
+Maintainer: [Lasse V. Karlsen][2]
+
+**NOTE**: This code is still very, *very*, much in flux. Do not use this
+in anything remotely related to production code. Expect methods to gain
+new parameters, be overloaded, or to disappear completely.
+
+**You have been warned!**
+
+  [1]: http://webdavsharp.codeplex.com/
+  [2]: mailto:lasse@vkarlsen.no
---
+++ WebDAVSharp.Server.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDAVSharp.Server.Tests")]
+[assembly: AssemblyDescription("Unit-tests for WebDAVSharp.Server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDAVSharp")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010-2011, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("13c006e4-2219-402a-997b-32d6da67f08a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
