e576cb8b1a12c21f35400d9843b749707286445d
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2011-02-12 16:38:49 +0100

    Initial commit, empty projects.

---
+++ WebDAVSharp.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDAVSharp.Server")]
+[assembly: AssemblyDescription("WebDAV Server implementation for WebDAV#")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDAVSharp")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010-2011, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("947fc4a0-9ed5-4d6c-9a97-9daf02524439")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("WebDAVSharp.Server.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f9927b1e8db3d03d8dfd517512b07d89a21c900dff7dfe4cd8f7bca7a5a3aee0a0d86e589e14fe0338dc3dd068f75702b6779c0a18fd1af7e8b701a54bcef80ef7055662d194ef0731bdbf809adcb6ea4c00f0b2b393dafe8c2f5550ea946bcc95924bb16d49a4e1e29c7e770e875109ce5278e2b46abcae8846d63b4f6732b9")]
